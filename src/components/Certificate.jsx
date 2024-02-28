5ffdd30d749881955e501108d9678c50eaec2e54
joshua <joshuapplg@gmail.com>
2024-02-28 09:53:36 +0700

    Dev#3

---
+++ src/components/Certificate.jsx
@@ -0,0 +1,17 @@
+
+function Certificate() {
+    return (
+        <div className="certificate">
+            <div className="certificate-content">
+            <div className="certificate-title">
+                <h2></h2>
+            </div>
+            <div className="certificate-content-show">       
+                
+            </div>
+            </div>
+        </div>
+    );
+}
+
+export default Certificate;
